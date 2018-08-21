4f0f2221dd2d01cfe4aa7b68a31f1ce47185127b
DeusMalsith <justinrashallphotography@gmail.com>
2018-08-21 12:07:32 -0700

    Added a comments page with comment adding functionality

---
+++ controllers/comments.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+
+router.post('/', function(req, res) {
+	console.log(req.body);
+	db.comment.create(req.body).then(function(createdComment) {
+		res.redirect('/articles/' + req.body.articleId);
+	}).catch(function(err) {
+		console.log(err);
+		res.render('error');
+	});
+});
+
+module.exports = router;
